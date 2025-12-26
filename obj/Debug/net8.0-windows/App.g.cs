9fb05c987bc7599764dd2ebcac5455ce2a0c37be
KaisAbiyyi <kaisabiyyi2@gmail.com>
2025-12-26 09:19:31 +0700

    chore: adding /obj to gitignore

--- obj/Debug/net8.0-windows/App.g.cs
+++ obj/Debug/net8.0-windows/App.g.cs
@@ -43,7 +43,7 @@ namespace MiniPhotoshop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.20.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
         public void InitializeComponent() {
             
             #line 4 "..\..\..\App.xaml"
@@ -58,7 +58,7 @@ namespace MiniPhotoshop {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.20.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
         public static void Main() {
             MiniPhotoshop.App app = new MiniPhotoshop.App();
             app.InitializeComponent();
